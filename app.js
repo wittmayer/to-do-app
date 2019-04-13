dde0d58b66df4bb3cc6cb2986e5a6d256b6026b3
Melissa <melissa.wittmayer@gmail.com>
2019-04-13 16:43:52 -0700

    built and added styling for to-do app

--- app.js
+++ app.js
@@ -6,20 +6,34 @@ function onReady() {
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
+    // create a new input
+    let checkbox = document.createElement('input');
+
     // get the text
     let title = newToDoText.value;
 
     // create a new li
     let newLi = document.createElement('li');
 
-    // create a new input
-    let checkbox = document.createElement('input');
+    // add delete button
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = "delete";
+    deleteButton.className = 'mdl-button mdl-js-button mdl-button--raised mdl-button--accent';
+    deleteButton.addEventListener('click', function(event){
+      toDoList.removeChild(this.parentElement);
+    })
+
 
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    // style checkbox
+    checkbox.className = 'mdl-checkbox__input'
+
+
     //set the title
     newLi.textContent = title;
+    newLi.className = 'mdl-list__item';
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
@@ -27,8 +41,12 @@ function onReady() {
     //attach the li to the ul
     toDoList.appendChild(newLi);
 
+    // attach the delete button to the li
+    newLi.appendChild(deleteButton);
+
     //empty the input
     newToDoText.value = '';
+
   });
 }
 
