db8d9c0f025e5bc508cc1d89737489ab2075c926
Melissa <melissa.wittmayer@gmail.com>
2019-04-14 11:53:56 -0700

    I DID IT

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function onReady() {
-  const toDos = [];
+  let id = 0;
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
@@ -7,15 +8,20 @@ function onReady() {
     if (!newToDoText.value) { return; }
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id,
     });
+
+    id++;
+
+    console.log(toDos);
+
     newToDoText.value = '';
     renderTheUI();
   }
 
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
-
     toDoList.textContent = '';
 
     toDos.forEach(function(toDo) {
@@ -23,11 +29,23 @@ function onReady() {
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
 
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = "delete";
+
+      deleteButton.addEventListener('click', event => {
+        toDos = toDos.filter(function(item){
+          return item.id !== toDo.id;
+        })
+
+        renderTheUI();
+      });
+
       newLi.textContent = toDo.title;
 
       toDoList.appendChild(newLi);
       newLi.appendChild(checkbox);
-    });
+      toDoList.appendChild(deleteButton);
+    })
   }
 
   addToDoForm.addEventListener('submit', event => {
